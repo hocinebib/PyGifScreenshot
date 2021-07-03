f8144b563748e33e0c3643b0da7f15924ab1a075
hocinebib <hocine.meraouna@gmail.com>
2021-07-03 20:44:23 +0200

    working version and changed file name

--- scr/PyGifScreenShot.py
+++ scr/PyGifScreenShot.py
@@ -8,29 +8,43 @@ First you need to have the python packages
 
 Then you can run the script with the following command :
 
-    python PyAnimScreenShot.py
+    python PyGifScreenShot.py
 
   Author
   ------
     Hocine Meraouna
 
 """
+import os
 import threading
 import pyautogui
 import PySimpleGUI as sg
 import cv2
+import imageio
 import Xlib
 from Xlib import display
 from collections import namedtuple
 
 
+layout = [[sg.InputText(size=(30, 1), key="File-Name", default_text='ScreenShot_Name')], [sg.Button('Generate'), sg.Button('Stop'), sg.Exit()]]
+
+window = sg.Window('PyScreenShot', layout, size=(500, 500),
+                   resizable=True, alpha_channel=0.4, background_color='grey')
+
+stop = True
+
+
 def get_active_window(disp, root, NET_ACTIVE_WINDOW):
+    """
+    Gives the active window in our case it will be the PyGifScreenShot window
+    """
     win_id = root.get_full_property(NET_ACTIVE_WINDOW,
                                     Xlib.X.AnyPropertyType).value[0]
     try:
         return disp.create_resource_object('window', win_id)
     except Xlib.error.XError:
         pass
+
 
 def get_absolute_geometry(win, root):
     """
@@ -50,16 +64,12 @@ def get_absolute_geometry(win, root):
     return (x, y+53, geom.width, geom.height-53)
 
 
-layout = [[sg.InputText(size=(30, 1), key="File-Name", default_text='ScreenShot_Name')], [sg.Button('Generate'), sg.Button('Stop'), sg.Exit()]]
-
-window = sg.Window('PyScreenShot', layout, size=(500, 500),
-                   resizable=True, alpha_channel=0.4, background_color='grey')
-
-
 def creating_screenshots():
     """
+    Creates the screenshots and turns them into an animated gif
     """
     i = 0
+    images = []
     while stop != True:
         disp = display.Display()
         root = disp.screen().root
@@ -73,27 +83,20 @@ def creating_screenshots():
         image = cv2.imread('Screenshots/'+values['File-Name']+str(i)+'.png')
         new = (image-40)*1.5
         cv2.imwrite('Screenshots/'+values['File-Name']+str(i)+'.png', new)
+        images.append(imageio.imread('Screenshots/'+values['File-Name']+str(i)+'.png'))
         i += 1
+    imageio.mimsave('Screenshots/'+values['File-Name']+'.gif', images)
 
-#d = display.Display()
-#root = d.screen().root
+    for j in range(i):
+        os.remove('Screenshots/'+values['File-Name']+str(j)+'.png')
 
-#query = root.query_tree()
-
-#for c in query.children:
-    # returns window name or None
-#    name = c.get_wm_name()
-#    if name: 
-#        print(name)
-
-stop = False
 
 while True:                             
     event, values = window.read()
 
     if event in (None, 'Exit'):
         break 
-    elif event == 'Generate':
+    elif (event == 'Generate') and (stop == True):
         stop = False
         threading.Thread(target=creating_screenshots, daemon=True).start()
     elif event == 'Stop':
